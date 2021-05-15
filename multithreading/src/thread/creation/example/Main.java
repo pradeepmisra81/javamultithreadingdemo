130522eeb7b095dbdbfd80c9c4543984722c326a
pradeep misra <pradeep.misra@kognitiv.com>
2021-05-15 23:15:27 +0530

    first commit for threads creation part1

--- multithreading/src/thread/creation/example/Main.java
+++ multithreading/src/thread/creation/example/Main.java
@@ -7,9 +7,14 @@ public class Main {
 			@Override
 			public void run() {
 				// code that will run in new thread
-				System.out.println("We are now in thread"+ Thread.currentThread().getName());
+				System.out.println("We are now in thread "+ Thread.currentThread().getName());
+				System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
 			}
 		});
+		
+		thread.setName("New Worker Thread");
+
+        thread.setPriority(Thread.MAX_PRIORITY);
 		
 		System.out.println("We are in thread "+ Thread.currentThread().getName()+ " before starting a new thread");
 
@@ -17,7 +22,6 @@ public class Main {
 		
 		System.out.println("We are in thread "+ Thread.currentThread().getName()+ " after starting a new thread");
 		
-		//Thread.sleep( millis: 1000);
 	}
 
 }
